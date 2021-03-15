3dd1af04e626a1da5a1347b230dc5c46293d3f32
ProjetoDSW <cleotads21@gmail.com>
2021-03-15 13:19:25 -0300

    Configurando banco do heroku test 4

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RNcaronas.config.base') #local
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RNcaronas.config.heroku') #dev
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RNcaronas.config.base') #local
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RNcaronas.config.heroku') #dev
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
